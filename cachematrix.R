9e8dd42bd60e9db89adabd9958f823d840e6471e
arpitad <arpita3892@gmail.com>
2015-06-21 15:03:45 +0530

    a pair of functions that cache the inverse of a matrix
    The first function helps in creating matrix which then sets it into list
    containing the functions to set the matrix, get the matrix, set the
    inverse of the matrix and get the inverse of the matrix.
    The second function checks if the inverse has already been calculated.
    If not, it calculates the inverse and gets the answer otherwise it
    retrieves the already calculated inverse

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function helps in creating matrix which then sets it into list containing the functions to set the matrix 
+## get the matrix, set the inverse of the matrix and get the inverse of the matrix
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix()) {original_matrix <<- x
+		inverse_matrix <<- NULL
+		set_matrix = function(z)
+		{	
+			original_matrix <<- z
+			inverse_matrix <<- NULL
+		}
+		get_matrix = function()
+		{
+			original_matrix
+		}
+		set_inverse = function(y = matrix())
+		{
+			inverse_matrix <<- y
+		}	
+		get_inverse = function()
+		{
+			inverse_matrix
+		}		
+		list( set_matrix = set_matrix, get_matrix = get_matrix, set_inverse = set_inverse, get_inverse = get_inverse)
+
 
 }
 
 
-## Write a short comment describing this function
+## This code checks if the matrix inverse has been calculated already
+## If not, then it calculates the inverse else retrieves the already calculated inverse
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) {if(!is.null(inverse_matrix))
+		{
+			message("Getting cached Inverse")
+		}
+		else
+		{	
+			message("Creating new Inverse")
+			inverse_matrix <<- solve(original_matrix)
+		}
+		inverse_matrix
+        
 }
